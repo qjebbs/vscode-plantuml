edacb6d45df10e965c5d6daf67a969a9409dc034
jebbs <qjebbs@gmail.com>
2017-05-07 12:50:22 +0800

    minimal zoom optimize

--- templates/js/zoom.js
+++ templates/js/zoom.js
@@ -14,16 +14,22 @@ window.onload = () => {
         let minWidth = contentWidth < naturalWidth ? contentWidth : naturalWidth;
         let minZoom = parseInt(minWidth / naturalWidth * 100);
 
+        if (minZoom == 100) return;
         zoom += event.wheelDelta / 12;
         if (zoom > 100) zoom = 100;
-        if (zoom < minZoom) zoom = minZoom;
-        let imgWidth = parseInt(naturalWidth * zoom / 100);
-        img.style.width = imgWidth + 'px';
-        let body = document.body;
-        let bodyWidth = imgWidth + marginPixels < winWidth ? winWidth : imgWidth + marginPixels;
-        body.style.width = bodyWidth + 'px'
-        if (body.offsetHeight < window.innerHeight) body.style.height = window.innerHeight - marginPixels + "px";
-
+        if (zoom < minZoom) {
+            zoom = minZoom;
+            img.style.width = "";
+            img.style.maxWidth = "";
+            document.body.style.width = "";
+        } else {
+            let imgWidth = parseInt(naturalWidth * zoom / 100);
+            img.style.width = imgWidth + 'px';
+            let body = document.body;
+            let bodyWidth = imgWidth + marginPixels < winWidth ? winWidth : imgWidth + marginPixels;
+            body.style.width = bodyWidth + 'px'
+            if (body.offsetHeight < window.innerHeight) body.style.height = window.innerHeight - marginPixels + "px";
+        }
         // let debugPar = document.getElementById("debug");
         // debugPar.innerHTML = `
         //     image naturalWidth:${naturalWidth}<br>
