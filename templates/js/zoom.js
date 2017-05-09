134497c33fdf1796883bc854bcdb4496c3ed6e43
jebbs <qjebbs@gmail.com>
2017-05-09 11:44:22 +0800

    zoom follow mouse pointer

--- templates/js/zoom.js
+++ templates/js/zoom.js
@@ -1,7 +1,8 @@
 window.onload = () => {
-    const marginPixels = 40;
+    const marginPixels = 20;
     let img = document.getElementById("image");
     let naturalWidth = img.naturalWidth;
+    let naturalHeight = img.naturalHeight;
     let zoom = (window.innerWidth - marginPixels) / naturalWidth * 100;
     if (zoom > 100) zoom = 100;
     document.body.addEventListener("mousewheel", zoomImage);
@@ -29,6 +30,8 @@ window.onload = () => {
         let minWidth = contentWidth < naturalWidth ? contentWidth : naturalWidth;
         let minZoom = parseInt(minWidth / naturalWidth * 100);
 
+        let mouseAt = getMousePointer();
+
         zoom += event.wheelDelta / 12;
         if (zoom > 100) zoom = 100;
         if (zoom < minZoom || minZoom == 100) {
@@ -44,7 +47,9 @@ window.onload = () => {
             let bodyWidth = imgWidth + marginPixels < winWidth ? winWidth : imgWidth + marginPixels;
             body.style.width = bodyWidth + 'px'
             if (body.offsetHeight < window.innerHeight) body.style.height = window.innerHeight - marginPixels + "px";
+            followMousePointer(mouseAt);
         }
+
         // let debugPar = document.getElementById("debug");
         // debugPar.innerHTML = `
         //     image naturalWidth:${naturalWidth}<br>
@@ -58,7 +63,25 @@ window.onload = () => {
         //     bodyWidth:${bodyWidth}<br>
         //     imgWidth:${imgWidth}<br>
         // `;
-
         return false;
+    }
+    function followMousePointer(mouseAt) {
+        let e = event || window.event;
+        let imgWidth = parseInt(naturalWidth * zoom / 100);
+        let imgHeight = parseInt(naturalHeight * zoom / 100);
+        document.body.scrollLeft += parseInt(imgWidth * mouseAt.imageX + marginPixels / 2) - mouseAt.x;
+        document.body.scrollTop += parseInt(imgHeight * mouseAt.imageY + marginPixels / 2) - mouseAt.y;
+    };
+    function getMousePointer() {
+        let imgWidth = parseInt(naturalWidth * zoom / 100);
+        let imgHeight = parseInt(naturalHeight * zoom / 100);
+        let e = event || window.event;
+        let mouseAt = {
+            x: e.clientX + document.body.scrollLeft,
+            y: e.clientY + document.body.scrollTop,
+            imageX: (e.clientX + document.body.scrollLeft - marginPixels / 2) / imgWidth,
+            imageY: (e.clientY + document.body.scrollTop - marginPixels / 2) / imgHeight,
+        }
+        return mouseAt;
     }
 }
