a7a63fd104984786ec2d3b774104c9e14ef0235c
jebbs <qjebbs@gmail.com>
2017-08-12 19:36:36 +0800

    fix vscode:prepublish

--- src/markdown-it-plantuml/index.ts
+++ src/markdown-it-plantuml/index.ts
@@ -1,8 +1,7 @@
-import * as markdowIt from 'markdown-it';
 import { renderHtml } from './render';
 import { plantumlWorker } from './rule';
 
-export function plantumlPlugin(md: markdowIt.MarkdownIt) {
+export function plantumlPlugin(md: any) {
     md.renderer.rules.plantuml = renderHtml;
     md.core.ruler.push("plantuml", plantumlWorker);
 }
