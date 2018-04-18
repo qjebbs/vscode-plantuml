383652ea046e04ac11861c2d5ac8eb82278dd927
jebbs <qjebbs@gmail.com>
2018-04-18 10:44:21 +0800

    remove unused includer code

--- src/plantuml/diagram/includer.ts
+++ src/plantuml/diagram/includer.ts
@@ -64,27 +64,5 @@ class Includer {
         if (fs.existsSync(p)) return [p];
         return [];
     }
-    private _canNotInclude(content: string): boolean {
-        let lines = content.split("\n");
-        let line1 = lines[0];
-        let line2 = "";
-        for (let i = 1; i < lines.length; i++) {
-            if (lines[i].trim()) {
-                line2 = lines[i];
-                break;
-            }
-        }
-        return isSalt(line1) || isSalt(line2)
-            || isEgg(line2) || isEarth(line2);
-        function isSalt(line: string): boolean {
-            return /^\s*salt\s*$/i.test(line) || /^\s*@startsalt/i.test(line);
-        }
-        function isEgg(line: string): boolean {
-            return lines.length == 3 && /^\s*(license|version|sudoku|listfonts|listopeniconic)/i.test(line);
-        }
-        function isEarth(line: string): boolean {
-            return /^\s*xearth\(\d+,\d+\)\s*$/i.test(line);
-        }
-    }
 }
 export const includer = new Includer();
