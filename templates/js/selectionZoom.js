b881abb27108c897f3a4e14a3b197f17944930a1
jebbs <qjebbs@gmail.com>
2018-05-16 16:27:23 +0800

    fix cannot reduce selection

--- templates/js/selectionZoom.js
+++ templates/js/selectionZoom.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
-    let img = document.getElementById("image");
+    let imgContainer = document.getElementById("image-container");
     let sBox = document.createElement("div");
-    document.body.appendChild(sBox);
+    imgContainer.appendChild(sBox);
     sBox.id = "selectionBox";
     sBox.style.display = "none";
     let start = {
@@ -17,7 +17,7 @@ window.addEventListener("load", () => {
         y: 0
     };
     let flag = false;
-    img.addEventListener("mousedown", e => {
+    imgContainer.addEventListener("mousedown", e => {
         if (e.buttons == 1) {
             flag = true;
             start.x = e.clientX;
@@ -29,7 +29,7 @@ window.addEventListener("load", () => {
             // console.log("select start", start);
         }
     });
-    img.addEventListener("mousemove", e => {
+    imgContainer.addEventListener("mousemove", e => {
         if (!flag) return;
         rectStart.x = Math.min(start.x, e.clientX);
         rectStart.y = Math.min(start.y, e.clientY);
