88212b4f5606788a5cf5e919ed81b4d9a69fffba
jebbs <qjebbs@gmail.com>
2018-12-05 13:00:14 +0800

    don't parse variables in notes

--- src/plantuml/intellisense/variableCompletion.ts
+++ src/plantuml/intellisense/variableCompletion.ts
@@ -5,6 +5,9 @@ import { diagramAt } from '../diagram/tools';
 
 const REG_VAR = /[0-9a-z_]+/ig;
 const REG_EXCLUDE_PREPROCESS = /^\s*!.+/i;
+const REG_REMOVE_INLINE_NOTE = /:.+$/i;
+const REG_ENTER_NOTE = /^\s*([rh]?note)(?:\s+(right|left|top|bottom))?(\s+(?:of|over))?[^:]+$/i;
+const REG_LEAVE_NOTE = /^\s*(end\s*[rh]?note)/i
 
 export async function VariableCompletionItems(target: vscode.TextDocument | Diagram, position: vscode.Position, token: vscode.CancellationToken): Promise<vscode.CompletionItem[]> {
     return new Promise<vscode.CompletionItem[]>((resolve, reject) => {
@@ -22,13 +25,25 @@ function collectVariables(target: vscode.TextDocument | Diagram, position: vscod
     let variables = new Set<string>([]);
     let diagram = target instanceof Diagram ? target : diagramAt(target, position);
     let excludeLine = position.line - diagram.start.line;
+    let flagNote = false;
     for (let i = 0; i < diagram.lines.length; i++) {
         if (i == excludeLine) continue;
         let line = diagram.lines[i];
         if (REG_EXCLUDE_PREPROCESS.test(line)) continue;
 
-        REG_VAR.lastIndex = 0;
+        if (flagNote) {
+            // currently in a note block
+            flagNote = !REG_LEAVE_NOTE.test(line);
+            continue;
+        }
+        flagNote = REG_ENTER_NOTE.test(line);
+
+        // FIXME: 
+        // Will remove none-note part if the line like:
+        // :Alice: -> :Bob:: notes
+        line = line.replace(REG_REMOVE_INLINE_NOTE, "");
         let matches: RegExpExecArray;
+        REG_VAR.lastIndex = 0;
         while (matches = REG_VAR.exec(line)) {
             variables.add(matches[0]);
         }
