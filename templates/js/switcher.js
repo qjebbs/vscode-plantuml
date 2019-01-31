4011961a8a0155e8f180ff6e63c3537192b190a2
jebbs <qjebbs@gmail.com>
2019-01-31 10:27:36 +0800

    fix page out of range, solve #184

--- templates/js/switcher.js
+++ templates/js/switcher.js
@@ -28,6 +28,7 @@ class Switcher {
     moveTo(page) {
         if (page < 1 || page > this.images.length) {
             console.log(`invalid page: target ${page}, range 1 - ${this.images.length}`);
+            if (page < 1) this.moveTo(1); else this.moveTo(this.images.length);
             return;
         }
         if (this.current == page) return;
