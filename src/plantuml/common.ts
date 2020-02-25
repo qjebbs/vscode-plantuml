f1187180d6d445e80dc76180d246ae32f07bb756
Michael Schulte <michael.schulte@gmx.org>
2020-02-25 20:58:29 +0100

    compile fix using any type

--- src/plantuml/common.ts
+++ src/plantuml/common.ts
@@ -9,4 +9,4 @@ export var bar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Lef
 export const extensionPath = vscode.extensions.getExtension("jebbs.plantuml").extensionPath;
 
 nls.config(<nls.Options>{ locale: vscode.env.language });
-export var localize: nls.LocalizeFunc = nls.loadMessageBundle(join(extensionPath, "langs", "lang.json"));
+export var localize: any = nls.loadMessageBundle(join(extensionPath, "langs", "lang.json"));
