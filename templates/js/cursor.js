d589153fcee9d6c5e4445409c75ff1b8776caa83
jebbs <qjebbs@gmail.com>
2024-03-28 11:47:32 +0800

    fix preview cursor

--- templates/js/cursor.js
+++ templates/js/cursor.js
@@ -1,29 +1,44 @@
 function addCursorManager(settings) {
-    let mouseMoveButton = settings.swapMouseButtons ? 0 : 2;
-
-    let mouseMoveButtonsValue = settings.swapMouseButtons ? 1 : 2;
-    let mouseZoomButtonsValue = settings.swapMouseButtons ? 2 : 1;
-
-    let defaultCursor = settings.swapMouseButtons ? "move" : "zoom-in";
-
-    let imgContainer= document.getElementById('image-container')
+    let imgContainer = document.getElementById('image-container')
+    let decideCursor = (e) => {
+        let leftButton = e.buttons & 1;
+        let rightButton = e.buttons & 2;
+        // console.log("leftButton", leftButton, "rightButton", rightButton);
+        if (!leftButton && !rightButton) {
+            if (settings.swapMouseButtons) {
+                imgContainer.style.cursor = "move";
+            } else if (e.altKey) {
+                imgContainer.style.cursor = "zoom-out";
+            } else {
+                imgContainer.style.cursor = "zoom-in";
+            }
+            return;
+        }
+        // zoom
+        if (
+            (leftButton && !settings.swapMouseButtons) ||
+            (rightButton && settings.swapMouseButtons)
+        ) {
+            if (e.altKey) {
+                imgContainer.style.cursor = "zoom-out";
+            } else {
+                imgContainer.style.cursor = "zoom-in";
+            }
+            return;
+        }
+        // move
+        imgContainer.style.cursor = "move";
+    }
     imgContainer.addEventListener("mousedown", e => {
-        // console.log(e);
-        if (e.buttons == mouseMoveButton) imgContainer.style.cursor = "move";
+        // console.log("mousedown", e.button, e.buttons);
+        decideCursor(e);
     });
     imgContainer.addEventListener("mousemove", e => {
-        // console.log(e);
-        if (e.buttons == mouseMoveButtonsValue) return;
-        if (e.buttons == mouseZoomButtonsValue) {
-            if (e.altKey)
-                imgContainer.style.cursor = "zoom-out";
-            else
-                imgContainer.style.cursor = "zoom-in";
-        }
-        if (e.buttons == 0) imgContainer.style.cursor = defaultCursor;
+        // console.log("mousemove", e.button, e.buttons);
+        decideCursor(e);
     });
     imgContainer.addEventListener("mouseup", e => {
-        // console.log(e);
-        imgContainer.style.cursor = defaultCursor;
+        // console.log("mousemove", e.button, e.buttons);
+        decideCursor(e);
     });
 };
